468060c0daf8812dc99118af7f1f44f14205cde6
Areej101 <69468368+Areej101@users.noreply.github.com>
2023-10-07 18:05:33 +0500

    upload all

--- main.js
+++ main.js
@@ -8,4 +8,13 @@ window.onload = function digital_fn(){
         collapse.classList.toggle('active');
         
     });
+
+    //masonry js
+    let grid = document.querySelector("#site-main .recent-work-area .images-flex");
+
+    let msnry = new Masonry(grid, {
+        itemSelector:'.flex-item',
+        gutter:100,
+        fitWidth:true,
+    })
 }
